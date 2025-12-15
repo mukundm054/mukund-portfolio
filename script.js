4d23adca3baa33d141a0fff1344c729f221dca32
mukundm054 <mukundm054@gmail.com>
2025-12-15 17:18:16 +0530

    my-portfolio
    Made using HTML ,CSS  , JAVASCRIPT and BOOTSTRAP its responsive

---
+++ script.js
@@ -0,0 +1,49 @@
+//contact alert button
+
+let op = document.getElementById("btn");
+let overlay = document.getElementById("alert");
+let cl = document.getElementById("close");
+
+op.addEventListener("click", () => {
+  overlay.style.display = "block";
+});
+
+cl.addEventListener("click", () => {
+  overlay.style.display = "none";
+});
+
+//read- more and read less
+
+let more = document.getElementById("more") 
+let goals = document.getElementById("goals")
+let goal = document.getElementById("goal")
+let Interests = document.getElementById("Interests")
+let activity = document.getElementById("activity")
+let why = document.getElementById("why")
+let portfolio = document.getElementById("portfolio")
+
+let less = document.getElementById("less")
+
+more.addEventListener('click',()=>{
+    more.style.display = "none"
+    goals.style.display = "block"
+    goal.style.display = "block"
+    Interests.style.display = "block"
+    activity.style.display = "block"
+    why.style.display = "block"
+    portfolio.style.display = "block"
+    less.style.display = "block"
+    
+})
+
+less.addEventListener("click",()=>{
+  more.style.display = "block"
+  goals.style.display = "none"
+  goal.style.display = "none"
+  Interests.style.display = "none"
+  activity.style.display = "none"
+  why.style.display = "none"
+  portfolio.style.display = "none"
+  less.style.display = "none"
+    
+})
